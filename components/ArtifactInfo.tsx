5dc85b54a435a2be03596f223bdf678d287077ca
psingh <pawandeep.singh@nextuple.com>
2023-08-08 12:48:43 -0400

    Initial commit for deployment

--- components/ArtifactInfo.tsx
+++ components/ArtifactInfo.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 
 export default async function ArticleInfo({ slug, title, image, body }: { slug: string, title: string, image: string, body: string }) {
     return (
         <div className='p-10 flex flex-col h-full w-full overflow-x-hidden'>
             <h1 className='pb-5 text-6xl text-slate-800'>{title}</h1>
-            <img src={image} width="340" height="230" className='mb-2 rounded '></img>
+            <Image src={image} width="340" height="230" className='mb-2 rounded ' alt="" />
             <article className="text-slate-500"
                 dangerouslySetInnerHTML={{ __html: body }} />
         </div>
